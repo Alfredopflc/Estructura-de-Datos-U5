665a47371dc56d7ca71d316b690c52eeeb2498f8
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-11-27 00:59:56 -0800

    Add files via upload

---
+++ 5.2-VenegasMedinaJoseAlfredo/5.2-VenegasMedinaJoseAlfredo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2_VenegasMedinaJoseAlfredo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickSort QS = new QuickSort(); //Se crea Objeto de la clase Quick Sort
+            int num = 0;
+            Console.Write("Cuantos elementos desea ingresar? \nR = "); //Se le puede pedir al usuario el tamano del arreglo
+            num = int.Parse(Console.ReadLine());
+
+
+            double[] Valores = new double[num]; //Arreglo base
+
+            double[] Arreglo1 = { 11, 2, 3, 14, 18, 4, 5, -3, -1, 6, 9 }; //Estos son los arreglos puestos en clase
+            double[] Arreglo2 = { 1, 3, 5, 7, 8, 3, 9, 0.7071, 16.5664, 12, 0, 10 };
+            double[] Arreglo3 = { 3, 7, 15, 1.3333, 155, 100, 15, 1.732, 5, -5, 3, 2 };
+            double[] Arreglo4 = { 8, 19, 7, 3, 15, 23, 34, 27, 89, 101 };
+ 
+            for(int i = 0; i < num; i++) //Este for es para ingresar los valores que se dean agregar
+            {
+                Console.Write("Ingrese numero: ");
+                Valores[i] = double.Parse(Console.ReadLine());
+            }
+
+            QS.Quicksort(Valores, 0, Valores.Length - 1); //Se hace llamar al Metodo Quick Sort a cada arreglo para ser ordenado
+            QS.Quicksort(Arreglo1, 0, Arreglo1.Length - 1);
+            QS.Quicksort(Arreglo2, 0, Arreglo2.Length - 1);
+            QS.Quicksort(Arreglo3, 0, Arreglo3.Length - 1);
+            QS.Quicksort(Arreglo4, 0, Arreglo4.Length - 1);
+
+            Console.WriteLine();
+            QS.Imprimir(Valores); //Se Imprime el arreglo ya ordenado
+            QS.Imprimir(Arreglo1);
+            QS.Imprimir(Arreglo2);
+            QS.Imprimir(Arreglo3);
+            QS.Imprimir(Arreglo4);
+
+            Console.ReadKey();
+            
+        }
+       
+    }
+}
---
+++ 5.2-VenegasMedinaJoseAlfredo/5.2-VenegasMedinaJoseAlfredo/QuickSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2_VenegasMedinaJoseAlfredo
+{
+    class QuickSort
+    {
+        public void Quicksort(double[] arreglo, int primero, int ultimo) //Metodo QuickSort
+        {
+
+            int i = primero, j = ultimo; //El valor de i empezara en 0, y el ultimo sera la ultima posicion del arreglo
+            int central = (i + j) / 2; //Se suman las posiciones, entre dos, para encontrar el elementos pivote
+            double pivote = arreglo[central]; //pivote sera dicho elemento
+
+            do
+            {
+                while (arreglo[i] < pivote) i++; //Si el elemento de la posicion i es menor al pivote, se le sumara 1 a i
+                while (arreglo[j] > pivote) j--; //Si el elemento de la posicion i es mayor al pivote, se le restara 1 a j
+
+                if (i <= j) //Si i es menor o igual que j se intercambian los valores
+                {
+                    double temp = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = temp;
+                    i++;
+                    j--;
+                }
+
+            } while (i <= j); //Se ciclara hasta que i sea menor o igual a j
+
+            if (primero < j) //Si primero es menor que j, se usara recursividad con los valores de primero y j
+                Quicksort(arreglo, primero, j);
+
+            if (i < ultimo) //Si i es menor que ultimo, se usara recursividad con los valores de ultimo e i
+                Quicksort(arreglo, i, ultimo);
+        }
+
+        public void Imprimir(double[] Datos) //Metodo para imprimir
+        {
+            for (int cont = 0; cont < Datos.Length; cont++) //Imprime el arreglo de forma ordenada
+            {
+                Console.Write(Datos[cont].ToString() + "  ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
