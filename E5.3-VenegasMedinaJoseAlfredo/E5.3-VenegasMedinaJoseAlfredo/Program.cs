95e0cda6dd61732087ac6f43573a0834257c688d
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-11-30 00:08:03 -0800

    Add files via upload

---
+++ E5.3-VenegasMedinaJoseAlfredo/E5.3-VenegasMedinaJoseAlfredo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E5._3_VenegasMedinaJoseAlfredo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            RadixSort RS = new RadixSort();
+
+            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            Console.WriteLine("\nOriginal array : ");
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+
+            RS.Sort(arr);
+            Console.WriteLine("\nSorted array : ");
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ E5.3-VenegasMedinaJoseAlfredo/E5.3-VenegasMedinaJoseAlfredo/RadixSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E5._3_VenegasMedinaJoseAlfredo
+{
+    class RadixSort
+    {
+        public void Sort(int[] arr)
+        {
+            int i, j;
+            int[] tmp = new int[arr.Length];
+
+            for (int shift = 31; shift >= 0; --shift)
+            {
+                j = 0;
+                for (i = 0; i < arr.Length; ++i)
+                {
+                    bool move = (arr[i] << shift) >= 0;
+                    if (shift == 0 ? !move : move)
+                        arr[i - j] = arr[i];
+                    else
+                        tmp[j++] = arr[i];
+                }
+                Array.Copy(tmp, 0, arr, arr.Length - j, j);
+            }
+        }
+    }
+}
