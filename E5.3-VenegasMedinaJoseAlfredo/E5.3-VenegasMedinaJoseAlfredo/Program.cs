7d6958998c9a4b330bf583b50d165909ec36f98c
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-12-02 18:39:56 -0800

    Add files via upload

--- E5.3-VenegasMedinaJoseAlfredo/E5.3-VenegasMedinaJoseAlfredo/Program.cs
+++ E5.3-VenegasMedinaJoseAlfredo/E5.3-VenegasMedinaJoseAlfredo/Program.cs
@@ -11,22 +11,39 @@ namespace E5._3_VenegasMedinaJoseAlfredo
         
         static void Main(string[] args)
         {
-            RadixSort RS = new RadixSort();
+            RadixSort RS = new RadixSort(); //Objeto de la Clase RadixSort
 
-            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
-            Console.WriteLine("\nOriginal array : ");
-            foreach (var item in arr)
-            {
-                Console.Write(" " + item);
-            }
+            int[] Lista1 = new int[] { 3, 6, 9, 5, 1, 4, 7, 2, 1, 3 }; //Listas asignadas en clase
+            int[] Lista2 = new int[] { 8, 3, 9, 3, 11, 7, 1, 27, 12 };
+            int[] Lista3 = new int[] { 10, 40, 36, 5, 24, 2, 5, 8 };
+            int[] Lista4 = new int[] { 55, 42, 0, -3, 0, -1, 2, 4, 7 };
+            int[] Lista5 = new int[] { 25, 108, 1024, 12, 351, 251, 39 };
 
-            RS.Sort(arr);
-            Console.WriteLine("\nSorted array : ");
-            foreach (var item in arr)
-            {
-                Console.Write(" " + item);
-            }
-            Console.WriteLine("\n");
+            //Se imprimen las listas en desorden
+            Console.WriteLine("\t\nListas en Desorden: ");
+            RS.Imprimir(Lista1);
+            RS.Imprimir(Lista2);
+            RS.Imprimir(Lista3);
+            RS.Imprimir(Lista4);
+            RS.Imprimir(Lista5);
+
+            //Acomodo de las listas por RadixSort
+            RS.Sort(Lista1);
+            RS.Sort(Lista2);
+            RS.Sort(Lista3);
+            RS.Sort(Lista4);
+            RS.Sort(Lista5);
+
+            //Se imprimen las listas en orden
+            Console.WriteLine();
+            Console.WriteLine("\t\nListas en Orden: ");
+            RS.Imprimir(Lista1);
+            RS.Imprimir(Lista2);
+            RS.Imprimir(Lista3);
+            RS.Imprimir(Lista4);
+            RS.Imprimir(Lista5);
+
+            Console.ReadKey();
         }
     }
 }
--- E5.3-VenegasMedinaJoseAlfredo/E5.3-VenegasMedinaJoseAlfredo/RadixSort.cs
+++ E5.3-VenegasMedinaJoseAlfredo/E5.3-VenegasMedinaJoseAlfredo/RadixSort.cs
@@ -8,24 +8,36 @@ namespace E5._3_VenegasMedinaJoseAlfredo
 {
     class RadixSort
     {
-        public void Sort(int[] arr)
+        public void Sort(int[] Arreglo) //Metodo para acomodar las listas
         {
             int i, j;
-            int[] tmp = new int[arr.Length];
+            int[] tmp = new int[Arreglo.Length]; //Metodo Temporal
 
-            for (int shift = 31; shift >= 0; --shift)
+            for (int cont = 31; cont >= 0; --cont) //Iniciamos for desde 31
             {
                 j = 0;
-                for (i = 0; i < arr.Length; ++i)
+                for (i = 0; i < Arreglo.Length; ++i) //Se evaluaran con respecto al tamano del arreglo
                 {
-                    bool move = (arr[i] << shift) >= 0;
-                    if (shift == 0 ? !move : move)
-                        arr[i - j] = arr[i];
-                    else
-                        tmp[j++] = arr[i];
+                    bool move = (Arreglo[i] << cont) >= 0; //Si es mayor o igual a 0, sera verdadero
+                    if (cont == 0 ? !move : move) //Evaluamos move y se compara con lo contrario
+                        Arreglo[i - j] = Arreglo[i]; //Intercambiamos valores
+
+                    else 
+                        tmp[j++] = Arreglo[i]; //Si no, lo movemos al arreglo auxiliar
                 }
-                Array.Copy(tmp, 0, arr, arr.Length - j, j);
+                Array.Copy(tmp, 0, Arreglo, Arreglo.Length - j, j);
+                //Array.Copy sirve para mover los valores de un arreglo a otro desde la cantidad que le sea asignada
+                //de modo: arreglo fuente, valor de inicio, arreglo destino, valor de destino, tamano de arreglo
             }
         }
+
+        public void Imprimir(int[] arr) //Metodo para imprimir las listas
+        {          
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }     
     }
 }
