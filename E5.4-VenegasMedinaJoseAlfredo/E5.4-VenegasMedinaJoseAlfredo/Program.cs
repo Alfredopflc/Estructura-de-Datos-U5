b56e5253647340f66053db0d1d914896da7ffa02
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-12-02 21:02:05 -0800

    Add files via upload

---
+++ E5.4-VenegasMedinaJoseAlfredo/E5.4-VenegasMedinaJoseAlfredo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E5._4_VenegasMedinaJoseAlfredo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShellSort SS = new ShellSort();
+            int[] arr = new int[] { 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            int n;
+
+            n = arr.Length;
+            Console.WriteLine("Original Array Elements :");
+            SS.show_array_elements(arr);
+            SS.shellSort(arr, n);
+            Console.WriteLine("\nSorted Array Elements :");
+            SS.show_array_elements(arr);
+            Console.ReadKey();
+        }
+    }
+}
